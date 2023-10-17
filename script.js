fbeaa56799f5c0d1450b5aa2d8af7a239374fa9f
Dante Ortiz <verlacdante@gmail.com>
2023-10-17 10:56:48 -0700

    Working on js

---
+++ script.js
@@ -0,0 +1,44 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password1 = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+//Error function
+function showError(input,message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
+//Success function
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+//email function
+function validEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(String(email).toLowerCase());
+}
+
+//validate field
+function validator(inputArr) {
+    inputArr.forEach(function(input) {
+        //console.log(input.value);
+        if(input.value.trim() === '') {
+            showError(input, 'is required');
+        } else {
+            showSuccess(input);
+        }
+    });
+}
+
+//Events
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+   validator([username, email, password1, password2]);
+});
